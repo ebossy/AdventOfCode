f585ff2823f11a02ffaed5211f106b33b47af49e
ebossy <enesyilmaz26@hotmail.de>
2024-12-08 01:45:25 +0100

    Day7

---
+++ 2024/7/7-1.py
@@ -0,0 +1,27 @@
+num=[]
+op = []
+with open("7-1.txt", "r") as datei:  # Encoding anpassen, falls nötig
+    for line in datei:
+        line = line.strip()
+        tmp1, tmp2 =line.split(":")
+        num.append(tmp1)
+        op.append(tmp2.strip().split(" "))
+
+summary = 0
+for i in range(len(num)):
+    length = 2 ** (len(op[i]) - 1)
+    stellen = len(op[i])-1
+    for k in range(length):
+        tmp = int(op[i][0])
+        binary = format(k,f'0{stellen}b')
+        for j in range(len(binary)):
+            if binary[j] == '1':
+                tmp += int(op[i][j+1])
+            else:
+                tmp *= int(op[i][j+1])
+        if tmp == int(num[i]):
+            summary += int(num[i])
+            break
+
+print(summary)
+
